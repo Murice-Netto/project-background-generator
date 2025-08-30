49bf0a18c68d0d326a852be0e63331fb7c50670b
Antonio Mauricio <mauricio.almeida.dev@gmail.com>
2025-08-30 11:18:30 -0300

    moves most of the main logic to separated functions in the utils.js

--- main.js
+++ main.js
@@ -6,18 +6,18 @@ import {
   GenerateBtn,
   GitHubUsernameInput,
   PreviewDiv,
-  ProjectDescription,
   ProjectDescriptionInput,
-  ProjectLogo,
   ProjectLogoInput,
-  ProjectTitle,
   ProjectTitleInput,
   RealNameInput,
-  UserInfo,
-  bgPattern,
 } from "./html-elements.js";
 
-import { fileToDataURL } from "./utils.js";
+import {
+  applySelectedGradientToPreviewBackground,
+  setBackgroundImagePattern,
+  setProjectInfo,
+  setProjectLogo,
+} from "./utils.js";
 
 GenerateBtn.addEventListener("click", async () => {
   const userInfo = {
@@ -33,51 +33,30 @@ GenerateBtn.addEventListener("click", async () => {
   };
 
   // Gradiente do preview
-  PreviewDiv.style.backgroundImage = `linear-gradient(to right, ${userInfo.colorOne}, ${userInfo.colorTwo}, ${userInfo.colorThree})`;
-  PreviewDiv.style.position = "relative"; // garante que as camadas fiquem no lugar
+  applySelectedGradientToPreviewBackground(PreviewDiv, [
+    userInfo.colorOne,
+    userInfo.colorTwo,
+    userInfo.colorThree,
+  ]);
 
   // Imagem de background pattern
-  if (userInfo.backgroundPatternImage) {
-    const bgDataUrl = await fileToDataURL(userInfo.backgroundPatternImage);
-    Object.assign(bgPattern.style, {
-      position: "absolute",
-      top: "0",
-      left: "0",
-      width: "100%",
-      height: "100%",
-      zIndex: "0",
-      pointerEvents: "none",
-      backgroundImage: `url('${bgDataUrl}')`,
-      backgroundRepeat: "repeat",
-      backgroundSize: "50px 50px",
-      opacity: "0.1",
-    });
-  }
+  if (userInfo.backgroundPatternImage)
+    await setBackgroundImagePattern(userInfo.backgroundPatternImage);
 
   // Conteúdo do preview
-  ProjectTitle.textContent = userInfo.projectTitle;
-  ProjectTitle.style.zIndex = 2;
-  ProjectDescription.textContent = userInfo.projectDescription;
-  ProjectDescription.style.zIndex = 2;
-  UserInfo.textContent = `${userInfo.githubUsername} — ${
+  setProjectInfo(
+    userInfo.projectTitle,
+    userInfo.projectDescription,
+    userInfo.githubUsername,
     userInfo.realName
-  } — @${new Date().getFullYear()}`;
-  UserInfo.style.zIndex = 2;
+  );
 
   // Logo do projeto
-  if (userInfo.projectLogo) {
-    const logoDataUrl = await fileToDataURL(userInfo.projectLogo);
-    ProjectLogo.style.backgroundImage = `
-      linear-gradient(to bottom right, ${userInfo.colorThree}, rgba(0,0,0, 0.1), ${userInfo.colorThree}),
-      url('${logoDataUrl}')
-    `;
-    ProjectLogo.style.backgroundSize = "cover";
-    ProjectLogo.style.backgroundPosition = "center";
-    ProjectLogo.style.zIndexbackgroundPosition = 2;
-  }
+  if (userInfo.projectLogo)
+    await setProjectLogo(userInfo.projectLogo, userInfo.colorThree);
 
   // Baixar preview
-  downloadPreview();
+  // downloadPreview();
 });
 
 function downloadPreview() {
--- utils.js
+++ utils.js
@@ -1,7 +1,65 @@
+import {
+  UserInfo,
+  bgPattern,
+  ProjectDescription,
+  ProjectLogo,
+  ProjectTitle,
+} from "./html-elements.js";
+
 export function fileToDataURL(file) {
   return new Promise((resolve) => {
     const reader = new FileReader();
+
     reader.onload = (e) => resolve(e.target.result);
+
     reader.readAsDataURL(file);
   });
+}
+
+export function applySelectedGradientToPreviewBackground(colors) {
+  PreviewDiv.style.backgroundImage = `linear-gradient(to right, ${colors.join(
+    ","
+  )})`;
+  PreviewDiv.style.position = "relative";
+}
+
+export async function setBackgroundImagePattern(patternImage) {
+  const bgDataUrl = await fileToDataURL(patternImage);
+
+  Object.assign(bgPattern.style, {
+    position: "absolute",
+    top: "0",
+    left: "0",
+    width: "100%",
+    height: "100%",
+    zIndex: "0",
+    pointerEvents: "none",
+    backgroundImage: `url('${bgDataUrl}')`,
+    backgroundRepeat: "repeat",
+    backgroundSize: "50px 50px",
+    opacity: "0.1",
+  });
+}
+
+export function setProjectInfo(title, description, githubUsername, realName) {
+  ProjectTitle.textContent = title;
+  ProjectTitle.style.zIndex = 2;
+
+  ProjectDescription.textContent = description;
+  ProjectDescription.style.zIndex = 2;
+
+  UserInfo.textContent = `${githubUsername} — ${realName} — @${new Date().getFullYear()}`;
+  UserInfo.style.zIndex = 2;
+}
+
+export async function setProjectLogo(logo, innerShadowColor) {
+  const logoDataUrl = await fileToDataURL(logo);
+
+  ProjectLogo.style.backgroundImage = `
+      linear-gradient(to bottom right, ${innerShadowColor}, rgba(0,0,0, 0.1), ${innerShadowColor}),
+      url('${logoDataUrl}')
+    `;
+  ProjectLogo.style.backgroundSize = "cover";
+  ProjectLogo.style.backgroundPosition = "center";
+  ProjectLogo.style.zIndexbackgroundPosition = 2;
 }
