664f07dec24fcd2cdaa035b4a34dbfafa6aeb5e1
Antonio Mauricio <mauricio.almeida.dev@gmail.com>
2025-08-31 17:41:29 -0300

    feat: adds exporting programming-language select field

--- js/html-elements.js
+++ js/html-elements.js
@@ -19,3 +19,6 @@ export const ProjectLogo = document.getElementById("project-logo");
 export const UserInfo = document.getElementById("user-info");
 export const bgPattern = document.getElementById("background-pattern");
 export const DownloadBtn = document.getElementById("download");
+export const PrgrammingLanguageSelect = document.getElementById(
+  "programming-language"
+);
